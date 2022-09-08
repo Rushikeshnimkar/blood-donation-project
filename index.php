e98b2fbacc057e2d7adcfe429a6d7d3ee682c689
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-09-08 21:43:35 +0530

    home page update

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blood Donation Project</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -25,15 +26,63 @@
                     <a class="nav-link text-bg-dark" href="request.php">Request</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-bg-dark" href="signin.php">signin</a>
+                    <a class="nav-link text-bg-dark" href="signin.php">SignIn</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-bg-dark" href="signin.php">signin</a>
+                    <a class="nav-link text-bg-dark" href="signup.php">SignUp</a>
                 </li>
 
             </ul>
         </div>
     </nav>
+
+    <main>
+        <div class="container body-container">
+            <div class="row justify-content-center">
+           
+                    <h3 class="w-50 p-2 bg-danger bg-gradient rounded-5 m-4 text-center">Welcome to the Blood Donation Camp!</h3>
+                    <h5 class="m-2 text-center fw-normal">Blood donation is a voluntary procedure that can help save lives. There are several types of blood donation. Each type helps meet different medical needs. The blood donation process from the time you arrive until the time you leave takes about an hour. The donation itself is only about 8-10 minutes on average.</h5>
+    
+            </div>
+
+            <div class="row justify-content-center">
+                <h3 class="w-25 p-2 bg-danger bg-gradient rounded-5 text-center">Why it's done?</h3>
+                <p class="text-center fs-5">You agree to have blood drawn so that it can be given to someone who needs a blood transfusion.
+                    <br>
+                    Millions of people need blood transfusions each year. Some may need blood during surgery. Others depend on it after an accident or because they have a disease that requires certain parts of blood. Blood donation makes all of this possible. There is no substitute for human blood — all transfusions use blood from a donor.
+
+                </p>
+            </div>
+
+            <div class="row justify-content-center">
+                <h3 class="w-25 p-2 bg-danger bg-gradient rounded-5 text-center">How it's done?</h3>
+            </div>
+
+            <div class="row justify-content-center">
+
+                <div class="col my-3 mx-nt">
+                    <img src="images/1.jpg" alt="donate.jpg" height="350">
+                </div>
+                <div class="col my-3">
+                    <ul>
+                        <li>If you’re donating whole blood, we’ll cleanse an area on your arm and insert a brand new sterile needle for the blood draw. (This feels like a quick pinch and is over in seconds.)</li>
+                        <li>
+                            Other types of donations, such as platelets, are made using an apheresis machine which will be connected to both arms.
+                        </li>
+                        <li>
+                            A whole blood donation takes about 8-10 minutes, during which you’ll be seated comfortably or lying down.
+                        </li>
+                        <li>
+                            When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.
+                        </li>
+                        <li>
+                            For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
     <script src="js/bootstrap.min.js"></script>
 </body>
 
---
+++ request.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Donation Project</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-dark ">
+        <div class="container-fluid">
+            <div class="container">
+                <a class="navbar-brand text-bg-dark" href="index.php">Blood Donations</a>
+            </div>
+
+            <ul class="nav justify-content-end d-flex flex-nowrap">
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark active" aria-current="page" href="donate.php">Donate</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="request.php">Request</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="signin.php">SignIn</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="signup.php">SignUp</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <main>
+        <div class="container-fluid body-container">
+            <div class="row this-row justify-content-center">
+                <div class="col-6">
+                    <h3>Request Blood!</h3>
+                </div>
+
+            </div>
+        </div>
+    </main>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
