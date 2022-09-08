308b97d08b7ebfd7ee0425713d2e0a152df40a3b
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-09-08 18:59:05 +0530

    navbar links

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,13 +8,33 @@
     <title>Blood Donation Project</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    
-        <nav class="navbar bg-dark">
+
+    <nav class="navbar navbar-expand-lg bg-dark ">
+        <div class="container-fluid">
             <div class="container">
-            <a class="navbar-brand text-bg-dark" href="index.php">Blood Donations</a>
+                <a class="navbar-brand text-bg-dark" href="index.php">Blood Donations</a>
             </div>
-        </nav>
+
+            <ul class="nav justify-content-end d-flex flex-nowrap">
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark active" aria-current="page" href="donate.php">Donate</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="request.php">Request</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="signin.php">signin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-bg-dark" href="signin.php">signin</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
     <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
